ce8b3c9e4ef65e403da3ad501886ae86b01270e2
daido1976 <daido1976@gmail.com>
2023-07-09 15:43:18 +0900

    Impl format_like_markdown_wasm in wasm with serde_wasm_bindgen

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,13 @@
-use wasm_bindgen::prelude::wasm_bindgen;
+use serde_wasm_bindgen::{from_value, to_value};
+use shared::{formatter::format_like_markdown, FileContentWithFileName};
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn format_like_markdown_wasm(data: JsValue) -> Result<JsValue, JsValue> {
+    let data: Vec<FileContentWithFileName> =
+        from_value(data).map_err(|err| JsValue::from_str(&err.to_string()))?;
+
+    let formatted = format_like_markdown(data);
+
+    to_value(&formatted).map_err(|err| JsValue::from_str(&err.to_string()))
 }
