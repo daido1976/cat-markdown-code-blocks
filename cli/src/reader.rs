6d30647be8d07e59a0b067da0c940c59db88933c
daido1976 <daido1976@gmail.com>
2023-11-11 07:45:43 +0900

    When a directory is passed in the CLI, all files under that directory are targeted

--- cli/src/reader.rs
+++ cli/src/reader.rs
@@ -3,6 +3,7 @@ use shared::formatter::FileContentWithFileName;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
+use walkdir::WalkDir;
 
 pub fn read_files<I, T>(files: I) -> Result<Vec<FileContentWithFileName>>
 where
@@ -11,30 +12,58 @@ where
 {
     files
         .into_iter()
-        .map(|filepath| {
-            let file = File::open(filepath.as_ref())?;
-            let reader = BufReader::new(file);
-
-            let filename = Path::new(filepath.as_ref())
-                .file_name()
-                .ok_or(anyhow::anyhow!("Invalid filename"))?
-                .to_string_lossy();
-
-            let file_content: Result<Vec<_>, _> = reader.lines().collect();
-            let file_content = file_content?.join("\n");
-            Ok((filename.to_string(), file_content))
+        .flat_map(|path| {
+            let path_ref = path.as_ref();
+            if path_ref.is_dir() {
+                to_boxed_iterator(
+                    WalkDir::new(path_ref)
+                        .into_iter()
+                        .filter_map(Result::ok)
+                        .filter(is_file)
+                        .map(|e| read_single_file(e.path())),
+                )
+            } else {
+                to_boxed_iterator(std::iter::once(read_single_file(path_ref)))
+            }
         })
         .collect()
+}
+
+fn to_boxed_iterator<I, T>(iterator: I) -> Box<dyn Iterator<Item = Result<T>>>
+where
+    I: Iterator<Item = Result<T>> + 'static,
+{
+    Box::new(iterator)
+}
+
+fn is_file(entry: &walkdir::DirEntry) -> bool {
+    entry.file_type().is_file()
+}
+
+fn read_single_file(path: &Path) -> Result<FileContentWithFileName> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let filename = path
+        .file_name()
+        .ok_or(anyhow::anyhow!("Invalid filename"))?
+        .to_string_lossy();
+
+    let file_content: Result<Vec<_>, _> = reader.lines().collect();
+    let file_content = file_content?.join("\n");
+
+    Ok((filename.to_string(), file_content))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
     use std::fs::write;
     use tempfile::tempdir;
 
     #[test]
-    fn test_read_files() {
+    fn test_read_files_with_file_path() {
         // Create a temporary directory.
         let dir = tempdir().unwrap();
 
@@ -61,5 +90,40 @@ mod tests {
 
         // Delete the directory and its content.
         dir.close().unwrap();
+    }
+
+    #[test]
+    fn test_read_files_with_directory_path() {
+        // Create a temporary directory.
+        let root_dir = tempdir().unwrap();
+
+        // Create a subdirectory.
+        let sub_dir = root_dir.path().join("sub");
+        fs::create_dir(&sub_dir).unwrap();
+
+        // Create files in the root directory.
+        let file_path1 = root_dir.path().join("file1.txt");
+        write(&file_path1, "Hello,").unwrap();
+
+        // Create files in the subdirectory.
+        let file_path2 = sub_dir.join("file2.txt");
+        write(&file_path2, "world\n!").unwrap();
+
+        // Call our function with the root directory.
+        let result = read_files(vec![root_dir.path().to_path_buf()]).unwrap();
+
+        // Sort results as file order is not guaranteed
+        let mut result_sorted = result.clone();
+        result_sorted.sort_by(|a, b| a.0.cmp(&b.0));
+
+        // Check the file content.
+        let expected = vec![
+            ("file1.txt".to_string(), "Hello,".to_string()),
+            ("file2.txt".to_string(), "world\n!".to_string()),
+        ];
+        assert_eq!(expected, result_sorted);
+
+        // Delete the directory and its content.
+        root_dir.close().unwrap();
     }
 }
