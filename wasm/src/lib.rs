3cb373e18bedb0029733e200aa32ddbb5800e514
daido1976 <daido1976@gmail.com>
2023-11-11 19:58:13 +0900

    Add todo comment

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,6 +2,8 @@ use serde_wasm_bindgen::{from_value, to_value};
 use shared::formatter::{format, MarkdownCodeBlock};
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
+// TODO: ts 側で型が format_wasm(data: any): any; になってしまうので、型が付くようにしたい
+// 今は type data = { file_name: string; content: string; }[] を明示的に指定する必要あり
 #[wasm_bindgen]
 pub fn format_wasm(data: JsValue) -> Result<JsValue, JsValue> {
     let data: Vec<MarkdownCodeBlock> =
