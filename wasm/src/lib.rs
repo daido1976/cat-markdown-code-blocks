ad9f24ffb043f01e1e4dafff96d63e0ccf78958a
daido1976 <daido1976@gmail.com>
2023-11-13 13:55:43 +0900

    Fix todo comment

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -2,8 +2,8 @@ use cmcb_core::formatter::{format, MarkdownCodeBlock};
 use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-// TODO: ts 側で型が format_wasm(data: any): any; になってしまうので、型が付くようにしたい
-// 今は type data = { file_name: string; content: string; }[] を明示的に指定する必要あり
+// TODO: Currently, the type in TypeScript is inferred as `format_wasm(data: any): any;`, we want the types to be automatically inferred more specifically.
+// At the moment, it's necessary to manually specify the type data as `{ file_name: string; content: string; }[]`
 #[wasm_bindgen]
 pub fn format_wasm(data: JsValue) -> Result<JsValue, JsValue> {
     let data: Vec<MarkdownCodeBlock> =
